c9fa5365c628cbc9e53b3a2e50f1ba5c3b2064c4
Joel Allen <joelallen720@gmail.com>
2021-01-31 14:21:35 -0500

    Update naming for keys (was conflict with win32 define). Update to reflect changes to vtk and ctk.

--- main.cc
+++ main.cc
@@ -5,16 +5,17 @@
 
 s32 main() {
     Core *core = create_core();
-    Platform *platform = create_platform(core->mem.perma);
+    Platform *platform = create_platform(&core->mem.perma);
     Vulkan *vulkan = create_vulkan(core);
-    ctk_visualize_stack(core->mem.perma, "perma");
-    ctk_visualize_stack(core->mem.temp, "temp");
 
     // Main Loop
     while (platform->window->open) {
+        // Handle Input
         process_events(platform->window);
-        if (key_down(platform, KEY_ESCAPE))
+        if (key_down(platform, INPUT_KEY_ESCAPE))
             break;
+
+        // ...
     }
 
     return 0;
--- vulkan.h
+++ vulkan.h
@@ -7,22 +7,22 @@
 #include "renderer/platform.h"
 
 struct Vulkan {
-    VTK_Instance *instance;
+    VTK_Instance instance;
 };
 
 static Vulkan *create_vulkan(Core *core) {
-    u32 temp_region = ctk_begin_region(core->mem.temp);
-    auto vulkan = ctk_push<Vulkan>(core->mem.perma, 1);
-    auto extensions = ctk_create_array<cstr>(core->mem.temp, 3);
-    auto layers = ctk_create_array<cstr>(core->mem.temp, 1);
-    // ctk_push(extensions, platform_get_vulkan_extension());
+    u32 temp_region = ctk_begin_region(&core->mem.temp);
+    auto vulkan = ctk_alloc<Vulkan>(&core->mem.perma, 1);
+    auto extensions = ctk_create_array<cstr>(&core->mem.temp, 16);
+    auto layers = ctk_create_array<cstr>(&core->mem.temp, 16);
+    ctk_concat(&extensions, PLATFORM_VULKAN_EXTENSIONS, PLATFORM_VULKAN_EXTENSION_COUNT);
     ctk_push(&extensions, VK_KHR_SURFACE_EXTENSION_NAME);
 
     // For validation layers.
     ctk_push(&extensions, VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-    ctk_push(&layers, "VK_LAYER_LUNARG_standard_validation");
+    ctk_push(&layers, "VK_LAYER_KHRONOS_validation");
 
-    vulkan->instance = vtk_create_instance(&extensions, &layers, core->mem.perma);
-    ctk_end_region(core->mem.temp, temp_region);
+    vulkan->instance = vtk_create_instance(&extensions, &layers);
+    ctk_end_region(&core->mem.temp, temp_region);
     return vulkan;
 }
