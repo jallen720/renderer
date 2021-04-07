d523aa735f321aaedf8735f5a06516517813dbbc
Joel Allen <joelallen720@gmail.com>
2021-04-06 21:35:49 -0400

    Just use bat for compiling shaders.

--- _sync_shaders.bat
+++ _sync_shaders.bat
@@ -1,2 +1,7 @@
 @echo off
-node.exe sync_shaders.js
+
+for /r %%v in (data\shaders\*.vert,data\shaders\*.frag) do (
+    if exist %%v.spv del %%v.spv
+	%home%\dev\lib\VulkanSDK\1.2.162.1\Bin32\glslc.exe %%v -o %%v.spv
+	echo compiled %%~nxv to %%~nxv.spv
+)
