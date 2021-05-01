4689becd00c2e2efdf284c3f103f7341cbdf2bfd
Joel Allen <joelallen720@gmail.com>
2021-04-30 23:09:27 -0400

    WIP

--- main.cc
+++ main.cc
@@ -3,15 +3,11 @@
 #include "ctk/math.h"
 
 struct App {
-    struct {
-        CTK_Stack *fixed;
-        CTK_Stack *temp;
-    } mem;
+    CTK_Stack *fixed_stack;
+    CTK_Allocator *fixed_alloc;
 
-    struct {
-        CTK_Allocator *fixed;
-        CTK_Allocator *temp;
-    } alloc;
+    CTK_Stack *temp_stack;
+    CTK_Allocator *temp_alloc;
 
     struct {
         Buffer *host;
@@ -56,6 +52,46 @@ static void allocate_regions(App *app, Vulkan *vk) {
     app->region.mesh = allocate_region(vk, app->buffer.host, 64, 64);
 }
 
+static void create_test_data(App *app, Vulkan *vk) {
+    app->vertexes = ctk_create_array<CTK_Vec3<f32>>(app->alloc.fixed, 64);
+    ctk_push(app->vertexes, { -0.4f, -0.4f, 0 });
+    ctk_push(app->vertexes, {  0,     0.4f, 0 });
+    ctk_push(app->vertexes, {  0.4f, -0.4f, 0 });
+
+    write_to_host_region(vk, app->region.mesh, app->vertexes->data, app->vertexes->count);
+}
+
+static void create_render_passes(App *app, Vulkan *vk) {
+    {
+        ctk_push_frame(app->temp_stack);
+
+        RenderPassInfo info = {};
+
+        info.attachment_descriptions = ctk_create_array<VkAttachmentDescription>(vk->temp_alloc, 1);
+
+        // Swapchain Image Attachment
+        ctk_push(info.attachment_descriptions, {
+            .flags = 0,
+            .format = vk->swapchain.image_format,
+            .samples = VK_SAMPLE_COUNT_1_BIT,
+            .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+            .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+            .stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+            .stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+            .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+            .finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+        });
+        ctk_push(&rp_info.clear_values, { 0, 0, 0, 1 });
+
+        info.subpass_infos = ctk_create_array<SubpassInfo>(vk->temp_alloc, 1);
+        info.subpass_dependencies = ctk_create_array<VkSubpassDependency>(vk->temp_alloc, 1);
+        info.framebuffer_infos = ctk_create_array<FramebufferInfo>(vk->temp_alloc, 1);
+        info.clear_values = ctk_create_array<VkClearValue>(vk->temp_alloc, 1);
+
+        ctk_pop_frame(app->temp_stack);
+    }
+}
+
 static App *create_app(CTK_Allocator *fixed_alloc, Vulkan *vk) {
     auto app = ctk_alloc<App>(fixed_alloc, 1);
     app->alloc.fixed = fixed_alloc;
@@ -64,17 +100,10 @@ static App *create_app(CTK_Allocator *fixed_alloc, Vulkan *vk) {
 
     create_buffers(app, vk);
     allocate_regions(app, vk);
+    create_test_data(app, vk);
+    create_render_passes(app, vk);
 
     return app;
-}
-
-static void test_data(App *app, Vulkan *vk) {
-    app->vertexes = ctk_create_array<CTK_Vec3<f32>>(app->alloc.fixed, 64);
-    ctk_push(app->vertexes, { -0.4f, -0.4f, 0 });
-    ctk_push(app->vertexes, {  0,     0.4f, 0 });
-    ctk_push(app->vertexes, {  0.4f, -0.4f, 0 });
-
-    write_to_host_region(vk, app->region.mesh, app->vertexes->data, app->vertexes->count);
 }
 
 s32 main() {
@@ -89,6 +118,7 @@ s32 main() {
     // Vulkan
     CTK_Stack *vulkan_stack = ctk_create_stack(base_alloc, 4 * CTK_MEGABYTE);
     CTK_Allocator *vulkan_alloc = ctk_create_allocator(vulkan_stack);
+
     VulkanInfo vulkan_info = {};
     vulkan_info.max_buffers = 2;
     vulkan_info.max_regions = 32;
@@ -96,9 +126,6 @@ s32 main() {
 
     // App
     App *app = create_app(base_alloc, vk);
-
-    // Setup Test Data
-    test_data(app, vk);
 
     // Main Loop
     while (platform->window->open) {
